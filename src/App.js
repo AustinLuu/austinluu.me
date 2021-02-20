ccd899ee41722566b8ae41e8edb77330c40a085c
Austin Luu <austinowenluu@gmail.com>
2021-02-20 18:01:38 -0500

    Boiler Code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from './components/Header'
-
+import About from './components/About'
 const App = () =>{
   return (
     <div className="App">
       <Header title = {"Austin Luu"}/>
+      <About/>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,25 @@ import PropTypes from 'prop-types'
 
 const Header = ({title}) => {
     return (
-        <header>
-            <h1>{title}</h1>
+        <header id='Header'>
+            <h1>Hi!</h1>
+            <h1>I'm {title}</h1>
+            <ul id='sub-nav'>
+                <li>
+                    <a href='#about'>/about</a>
+                </li>
+                <li>
+                    <a href='#experience'>/prev</a>
+                </li>
+                <li>
+                    <a href='#projects'>/projects</a>
+                </li>
+                <li>
+                    <a href='#blog'>/blog</a>
+                </li>
+            </ul>
         </header>
+
     )
 }
 
