b749ce90d684c6147415acd801dbbeed429b129f
Austin Luu <austinowenluu@gmail.com>
2021-02-21 23:11:30 -0500

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -8,15 +8,16 @@ const About = () => {
                 <p>Welcome to my little corner of the internet! I’m Austin, a curious engineer interested in medical robotics, ml, and accessible tech — anything that can make technology work better for everyone. Currently, I'm based in Toronto, Canada studying Mechatronics Engineering @ryu.</p>
                 <p>In my spare time, you can find me power lifting at the gym, shooting mediocre photos, or at your local beach playing volleyball! Recently, I've started a podcast on which I plan to talk more about some of my thoughts on tech and general musings about the world.</p>
                 <p>
-                    Feel free to download my <a target="_blank" rel="noreferrer" href="./static/Austin_Luu_Resume_2021_C_V1.pdf">resume</a>,
-                    poke around <a target="_blank" rel="noreferrer" href="https://github.com/AustinLuu">my GitHub</a>, or
-                    contact me for a chat at <a target="_blank" rel="noreferrer"
+                    Feel free to download my <a target="_blank" rel="noopener noreferrer" href="./static/Austin_Luu_Resume_2021_C_V1.pdf">resume</a>,
+                    poke around <a target="_blank" rel="noopener noreferrer" href="https://github.com/AustinLuu">my GitHub</a>, or
+                    contact me for a chat at <a target="_blank" rel="noopener noreferrer"
                         href="mailto:austinowenluu@gmail.com">austinowenluu@gmail.com</a>. I'm always happy to meet new people!
                 </p>
             </div>
             <div>
                 <img className='me' src='./static/IMG-20201009-WA0001_2.jpg' alt='Austin Luu' />
                 <table>
+                    <tbody>
                     <tr>
                         <td>
                             <pre>
@@ -43,6 +44,7 @@ const About = () => {
                             </pre>
                         </td>
                     </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
