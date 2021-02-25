eada918e8b39c5e97815c5033abf1515f1fbca64
Austin Luu <austinowenluu@gmail.com>
2021-02-24 19:59:05 -0500

    Button Styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 const Projects = () => {
     return (
-        <div>
-            
+        <div className='lt-content-column' id='about' data-aos="fade-up"data-aos-duration="750"data-aos-easing="custom"data-aos-offset="100">
+            <h2><span className="side-header">Projects</span></h2>
         </div>
     )
 }
