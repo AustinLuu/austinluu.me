a0d6409310219f4eff4048cb275b6d1705124703
Austin Luu <austinowenluu@gmail.com>
2021-03-05 20:38:50 -0500

    mvp project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -131,7 +131,7 @@ const Projects = () => {
     ])
 
     return (
-        <div className='lt-content-column' id='about' data-aos="fade-up"data-aos-duration="750"data-aos-easing="custom"data-aos-offset="100">
+        <div className='lt-content-column' id='projects' data-aos="fade-up"data-aos-duration="750"data-aos-easing="custom"data-aos-offset="100">
             <h2><span className="side-header">Projects</span></h2>
             <Accordion allowToggle>
             {projects.map(({title, brief, description,tools})=>
